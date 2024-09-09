ce9c8cb6ee0078c1732095ce8a9da41978fb6087
Therf13 <157944898+Therf13@users.noreply.github.com>
2024-09-09 14:08:12 +0000

    Done

--- Tasksheet126.java
+++ Tasksheet126.java
@@ -1,1 +1,30 @@
+abstract class abstractClass {
+    abstract void abstractMethod();
+    void concreteMethod() { // concrete methods are still allowed in abstract classes
+        System.out.println("This is a concrete method.");
+    }
+}
+
+class B extends abstractClass {
+    void abstractMethod() {
+        System.out.println("B's implementation of abstractMethod.");
+    }
+}
+
+class C extends abstractClass {
+    void abstractMethod () {
+        System.out.println("C's implentation of abstractMethod.");
+    }
+}
+
+public class Tasksheet126{
+    public static void main(String args[]) {
+        B b = new B();
+        b.abstractMethod();
+        b.concreteMethod();
+        C c = new C();
+        c.abstractMethod();
+        c.concreteMethod();
+    }
+}
 
